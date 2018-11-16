54ff284a9ddb24d3d2ca6fd1050361a8588ecc9b
mayankmetha <mayankmetha@gmail.com>
2018-11-16 22:20:36 +0530

    fix controller for images

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -1,6 +1,6 @@
 
 // Import only what we need from express
-import { ImageDetails, } from '../model';
+import { ImageDetails, CustomError } from '../model';
 import { Route, Get, Post, Body, Query, SuccessResponse, Response, Controller, Delete } from 'tsoa';
 import { ImageManager } from '../service';
 
@@ -13,6 +13,21 @@ export class ImageController extends Controller {
     public async getAllImages(userId: string): Promise<ImageDetails[]> {
         const images = await this.imageManager.getAllImages(userId).toPromise();
         return images;
+    }
+
+    @Response('500', 'If the image Location requested already exists')
+    @Response('400', 'If any required fields are missing in the request')
+    @SuccessResponse('201', 'Created')
+    @Post('users/{userId}/images')
+    public async uploadImage(@Body() requestBody: ImageDetails): Promise<void> {
+        try {
+            var data = await this.imageManager.createImage(requestBody).toPromise();
+            this.setStatus(201);
+        } catch (error) {
+            console.error('Failed to upload image: ', error);
+            this.setStatus(500);
+            throw new CustomError(500, error);
+        }
     }
 
     private imageManager: ImageManager = new ImageManager();
