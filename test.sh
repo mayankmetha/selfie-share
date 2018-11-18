603ac9bd9836629c1e3699ecf79374b7cfb68046
mayankmetha <mayankmetha@gmail.com>
2018-11-18 12:00:40 +0530

    Fix random test order

---
+++ test.sh
@@ -0,0 +1,4 @@
+mocha --require ts-node/register --report list test/users.test.ts
+mocha --require ts-node/register --report list test/friends.test.ts
+mocha --require ts-node/register --report list test/images.test.ts
+mocha --require ts-node/register --report list test/shared.images.test.ts
